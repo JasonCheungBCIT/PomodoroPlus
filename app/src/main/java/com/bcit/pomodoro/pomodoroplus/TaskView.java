985e61b2b73fda42d14b65eb08ead2f32c094eea
Jason Cheung <jayson.cheung@hotmail.com>
2016-02-01 22:18:13 -0800

    Finished the view tasks layout.
    Man I hate colors.

---
+++ app/src/main/java/com/bcit/pomodoro/pomodoroplus/TaskView.java
@@ -0,0 +1,124 @@
+package com.bcit.pomodoro.pomodoroplus;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Jason Cheung on 2016-02-01.
+ */
+public class TaskView extends RelativeLayout {
+
+    /* -- FIELDS -- */
+    protected RelativeLayout taskView;
+    protected TextView title, category, duration;
+    protected long timeLeft;  // TODO: Investigate holding time-left or just the deadline.
+    protected int bgColor;    // Stored in res/values/colors
+
+
+    /* -- CONSTRUCTOR -- */
+    public TaskView (Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TaskView (Context context, AttributeSet attrs,
+                     String title, String category, long timeLeft, int color) {
+        super(context, attrs);
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        inflater.inflate(R.layout.task_view, this);
+
+        taskView = (RelativeLayout) findViewById(R.id.task_view);
+
+        this.title = (TextView) findViewById(R.id.task_title);
+        this.category = (TextView) findViewById(R.id.category);
+        this.duration = (TextView) findViewById(R.id.duration);
+
+        this.title.setText(title);
+        this.category.setText(category);
+        this.duration.setText(Long.toString(timeLeft / 1000 / 60) + "min");
+        this.taskView.setBackgroundColor(color);
+    }
+
+    /* -- GETS AND SETS -- */
+    // TODO: Doesn't actually update the view
+    public TextView getTitle() {
+        return title;
+    }
+
+    public void setTitle(TextView title) {
+        this.title = title;
+    }
+
+    public TextView getCategory() {
+        return category;
+    }
+
+    public void setCategory(TextView category) {
+        this.category = category;
+    }
+
+    public TextView getDuration() {
+        return duration;
+    }
+
+    public void setDuration(TextView duration) {
+        this.duration = duration;
+    }
+
+    public int getBgColor() {
+        return bgColor;
+    }
+
+    public void setBgColor(int bgColor) {
+        this.bgColor = bgColor;
+    }
+}
+
+// TODO: this stupid color system.
+// Note: Bottom two don't set color.
+class ExerciseView extends TaskView {
+
+    public ExerciseView (Context context, AttributeSet attrs,
+                     String title, String category, long timeLeft) {
+        super(context, attrs);
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        inflater.inflate(R.layout.exercise_view, this);
+
+        taskView = (RelativeLayout) findViewById(R.id.task_view);
+
+        this.title = (TextView) findViewById(R.id.task_title);
+        this.category = (TextView) findViewById(R.id.category);
+        this.duration = (TextView) findViewById(R.id.duration);
+
+        this.title.setText(title);
+        this.category.setText(category);
+        this.duration.setText(Long.toString(timeLeft / 1000 / 60) + "min");
+    }
+}
+
+class BreakView extends TaskView {
+    public BreakView (Context context, AttributeSet attrs,
+                         String title, String category, long timeLeft) {
+        super(context, attrs);
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        inflater.inflate(R.layout.break_view, this);
+
+        taskView = (RelativeLayout) findViewById(R.id.task_view);
+
+        this.title = (TextView) findViewById(R.id.task_title);
+        this.category = (TextView) findViewById(R.id.category);
+        this.duration = (TextView) findViewById(R.id.duration);
+
+        this.title.setText(title);
+        this.category.setText(category);
+        this.duration.setText(Long.toString(timeLeft / 1000 / 60) + "min");
+    }
+}
+
---
+++ app/src/main/java/com/bcit/pomodoro/pomodoroplus/TaskViewerActivity.java
@@ -0,0 +1,133 @@
+package com.bcit.pomodoro.pomodoroplus;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by Jason Cheung on 2016-02-01.
+ */
+public class TaskViewerActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_viewer);
+
+        // Navigation
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        // End navigation
+
+        LinearLayout taskHolder = (LinearLayout) findViewById(R.id.task_holder);
+
+        /*
+        RelativeLayout t1 = new TaskView(getApplicationContext(), null);
+         */
+
+        taskHolder.addView(new TaskView(getApplicationContext(), null,
+                "Android Dev", "Science", 15 * 60 * 1000, ContextCompat.getColor(getApplicationContext(), R.color.taskRed)));
+        taskHolder.addView(new TaskView(getApplicationContext(), null,
+                "Literature", "Arts", 15 * 60 * 1000, ContextCompat.getColor(getApplicationContext(), R.color.taskIndigo)));
+        taskHolder.addView(new ExerciseView(getApplicationContext(), null,
+                "Push-ups!", "Exercise", 15 * 60 * 1000));
+        taskHolder.addView(new TaskView(getApplicationContext(), null,
+                "Project UI Design", "Creative", 15 * 60 * 1000, ContextCompat.getColor(getApplicationContext(), R.color.taskViolet)));
+        taskHolder.addView(new TaskView(getApplicationContext(), null,
+                "Android Dev", "Science", 15 * 60 * 1000, ContextCompat.getColor(getApplicationContext(), R.color.taskRed)));
+        taskHolder.addView(new BreakView(getApplicationContext(), null,
+                "Relax", "Break", 15 * 60 * 1000));
+    }
+
+
+    /* -- NAVIGATION -- */
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.task_viewer_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
