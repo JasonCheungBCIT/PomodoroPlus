0055e97bdce2dbcddc0cca595367025565785b9e
Jordan Hamade <ardf2014@hotmail.com>
2016-02-08 18:56:28 -0800

    Added settings
    SettingActivity.java has been added but not yet linked (settings page).
    arrays.xml and preferences.xml have been added (do not need to be
    touched)

--- app/src/main/java/com/bcit/pomodoro/pomodoroplus/MainActivity.java
+++ app/src/main/java/com/bcit/pomodoro/pomodoroplus/MainActivity.java
@@ -1,8 +1,8 @@
 package com.bcit.pomodoro.pomodoroplus;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
---
+++ app/src/main/java/com/bcit/pomodoro/pomodoroplus/SettingActivity.java
@@ -0,0 +1,25 @@
+package com.bcit.pomodoro.pomodoroplus;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jordan H on 2016-02-08.
+ */
+public class SettingActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MainSettingsFragment()).commit();
+    }
+    public static class MainSettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+
+}
