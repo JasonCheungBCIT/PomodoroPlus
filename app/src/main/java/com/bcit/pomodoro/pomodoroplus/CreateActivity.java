5d9ab7d87c090429dbe222ccbdba03d5c0366bde
Manjot Khalsa <manjotkhalsa.96@gmail.com>
2016-02-08 23:09:37 -0800

    Added Spinners
    the colours are terrible; I’ll update them soon; Spinners are good(i
    think) gotta find a better way to get duration

--- app/src/main/java/com/bcit/pomodoro/pomodoroplus/CreateActivity.java
+++ app/src/main/java/com/bcit/pomodoro/pomodoroplus/CreateActivity.java
@@ -1,13 +1,38 @@
 package com.bcit.pomodoro.pomodoroplus;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class CreateActivity extends AppCompatActivity {
+
+    private Spinner sCategory, sPriority, sColor;
+    private ArrayAdapter<String> categoryAdapter, priorityAdapter, colorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        final Resources res;
+        final String[] categoryArray, colorArray, priorityArray;
+
+        sCategory       = (Spinner) findViewById(R.id.spinCategory);
+        sPriority       = (Spinner) findViewById(R.id.spinPriority);
+        sColor          = (Spinner) findViewById(R.id.spinColor);
+        res             = getResources();
+        categoryArray   = res.getStringArray(R.array.category_arrays);
+        colorArray      = res.getStringArray(R.array.color_arrays);
+        priorityArray   = res.getStringArray(R.array.priority_arrays);
+        categoryAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, categoryArray);
+        priorityAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, priorityArray);
+        colorAdapter    = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, colorArray);
+
+        //categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sCategory.setAdapter(categoryAdapter);
+        sColor.setAdapter(colorAdapter);
+        sPriority.setAdapter(priorityAdapter);
     }
 }
