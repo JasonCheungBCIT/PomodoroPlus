5fed613cdc5f7c67463ad71a4e2855396a2506dc
Jason Cheung <jayson.h.cheung@gmail.com>
2016-02-13 15:56:43 -0800

    Added basic/loading of tasks.
    Click "Jason Testing Area" to test my code. Basically saves a test array
    of TaskModels, and loads them to my test activity.

---
+++ app/src/main/java/com/bcit/pomodoro/pomodoroplus/JasonTestBench.java
@@ -0,0 +1,156 @@
+package com.bcit.pomodoro.pomodoroplus;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/**
+ * Created by Jason Cheung on 2016-02-13.
+ */
+public class JasonTestBench extends Activity {
+    private static final String TAG = "JasonTestBench";
+    private static final String FILE_NAME = "stored_tasks";
+
+    private LinearLayout taskHolder;
+    private TextView fileDisplay;
+    private String[] allFiles;
+
+    private int counter = 0;    // Testing: Files are overwritten if the names are the same.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jason_test_bench);
+
+        taskHolder = (LinearLayout) findViewById(R.id.jason_task_holder);
+        fileDisplay = (TextView) findViewById(R.id.file_display);
+    }
+
+    private void displayAllFiles() {
+        allFiles = getApplicationContext().fileList();
+        StringBuilder fatString = new StringBuilder();
+        for (String s : allFiles) {
+            fatString.append(s + " ");
+        }
+        fileDisplay.setText(fatString.toString());
+    }
+
+    private static ArrayList<TaskModel> generateTestModels(Context context) {
+
+        ArrayList<TaskModel> models = new ArrayList<>();
+
+        models.add(new TaskModel("Android Dev", "Science", 15 * 60 * 1000,
+                ContextCompat.getColor(context, R.color.taskRed)));
+        models.add(new TaskModel("Literature", "Arts", 15 * 60 * 1000,
+                ContextCompat.getColor(context, R.color.taskOrange)));
+        models.add(new TaskModel("Push-ups!", "Exercise", 5 * 60 * 1000,
+                ContextCompat.getColor(context, R.color.taskYellow)));
+        models.add(new TaskModel("Project UI Design", "Creative", 15 * 60 * 1000,
+                ContextCompat.getColor(context, R.color.taskGreen)));
+        models.add(new TaskModel("Relax", "Break", 30 * 60 * 1000,
+                ContextCompat.getColor(context, R.color.taskBlue)));
+        models.add(new TaskModel("Foo", "Bar", 15 * 60 * 1000,
+                ContextCompat.getColor(context, R.color.taskIndigo)));
+
+        return models;
+    }
+
+    public void onCreateFileClick(View view) {
+        counter++;
+
+        String FILENAME = FILE_NAME + counter;
+
+        // Generate test data to save
+        ArrayList<TaskModel> testModels = generateTestModels(getApplicationContext());
+        SavePackage save = new SavePackage(testModels);
+
+        // Serialize test data
+        Gson gson = new Gson();
+        String content = gson.toJson(save);
+
+        // Save to file
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            fos.write(content.getBytes());
+            fos.close();
+        } catch (FileNotFoundException e) {
+            Log.d("FILE", "Failed to create file");
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.d("FILE", "Failed to write file");
+            e.printStackTrace();
+        }
+
+        displayAllFiles();
+    }
+
+    public void onLoadFileClick(View view) {
+        String FILENAME = FILE_NAME + counter;
+        StringBuilder jsonContent = new StringBuilder();
+
+        // Retrieve content from file
+        FileInputStream fis = null;
+        try {
+            fis = openFileInput(FILENAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+            while ((line = br.readLine()) != null) {
+                jsonContent.append(line);
+            }
+            fis.close();
+        } catch (FileNotFoundException e) {
+            Log.d("FILE", "Failed to create file");
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.d("FILE", "Failed to write file");
+            e.printStackTrace();
+        }
+
+        // Convert back to object
+        SavePackage save;
+        Gson gson = new Gson();
+        save = gson.fromJson(jsonContent.toString(), SavePackage.class);
+
+        // Load the object
+        for (TaskModel tm : save.getTaskModels()) {
+            taskHolder.addView(new TaskView(getApplicationContext(), null, tm));
+        }
+
+        displayAllFiles();
+    }
+}
+
+/**
+ * Wraps our models to allow serialization.
+ */
+class SavePackage {
+
+    private ArrayList<TaskModel> taskModels;
+
+    SavePackage(ArrayList<TaskModel> models) {
+        taskModels = models;
+    }
+
+    public ArrayList<TaskModel> getTaskModels() {
+        return taskModels;
+    }
+}
--- app/src/main/java/com/bcit/pomodoro/pomodoroplus/MainActivity.java
+++ app/src/main/java/com/bcit/pomodoro/pomodoroplus/MainActivity.java
@@ -1,9 +1,15 @@
 package com.bcit.pomodoro.pomodoroplus;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,4 +38,10 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), SettingActivity.class);
         startActivity(intent);
     }
+
+    public void onJasonClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), JasonTestBench.class);
+        startActivity(intent);
+    }
+
 }
