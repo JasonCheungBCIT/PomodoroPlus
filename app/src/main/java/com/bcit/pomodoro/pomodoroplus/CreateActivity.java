a838302ca3be15b64f0b09dfa0e070019b158e13
Jason Cheung <jayson.h.cheung@gmail.com>
2016-03-07 23:03:13 -0800

    Revamp create tasks, connected to dates, added color picker.
    Revamp create tasks
    - New visuals
    - Loads any existing tasks for that date
    - Allows adding to current task set
    - proper saving of tasks
    Connected to dates
    - Date picker activity leads to the create tasks
    - Passes the right info to the next activity
    Color Picker
    - Added a new package Color picker, more details here
    https://github.com/kristiyanP/colorpicker

--- app/src/main/java/com/bcit/pomodoro/pomodoroplus/CreateActivity.java
+++ app/src/main/java/com/bcit/pomodoro/pomodoroplus/CreateActivity.java
@@ -3,7 +3,6 @@ package com.bcit.pomodoro.pomodoroplus;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -38,6 +38,10 @@ public class CreateActivity extends AppCompatActivity {
     private static final String  FILE_NAME = "stored_tasks";
     private String              fileName;
     private ColorPicker         colorPicker;
+    private LinearLayout taskHolder;
+
+    private ArrayList<TaskModel> tasks;
+    private int                  selectedColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +65,8 @@ public class CreateActivity extends AppCompatActivity {
         priorityAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, priorityArray);
         colorAdapter    = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, colorArray);
 
+        taskHolder      = (LinearLayout) findViewById(R.id.task_holder);
+
         //categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sCategory.setAdapter(categoryAdapter);
         // sColor.setAdapter(colorAdapter);
@@ -70,14 +76,19 @@ public class CreateActivity extends AppCompatActivity {
         colorPicker.setRoundButton(true);
         colorPicker.setColors(
                 ContextCompat.getColor(context, R.color.taskRed),
+                ContextCompat.getColor(context, R.color.taskOrange),
+                ContextCompat.getColor(context, R.color.taskYellow),
                 ContextCompat.getColor(context, R.color.taskGreen),
                 ContextCompat.getColor(context, R.color.taskBlue),
+                ContextCompat.getColor(context, R.color.taskIndigo),
                 ContextCompat.getColor(context, R.color.taskViolet));
 
         colorPicker.setFastChooser(new ColorPicker.OnFastChooseColorListener() {
             @Override
             public void setOnFastChooseColorListener(int position, int color) {
                 Toast.makeText(context, "color:" + color, Toast.LENGTH_SHORT).show();
+                selectedColor = color;
+                colorPicker.setDefaultColor(color);
                 colorPicker.dismissDialog();
             }
         });
@@ -107,6 +118,59 @@ public class CreateActivity extends AppCompatActivity {
                 getIntent().getIntExtra("month", 1),
                 getIntent().getIntExtra("year", 2000)
         );
+
+        // Load any data from this date
+        tasks = loadDataFromDate(fileName);
+        if (tasks == null) {
+            tasks = new ArrayList<>();
+        }
+        displayAllTasks(tasks);
+    }
+
+    private ArrayList<TaskModel> loadDataFromDate(String date) {
+
+        StringBuilder jsonContent = new StringBuilder();
+        File fileForDate = new File(getApplicationContext().getFilesDir(), date);
+
+        try {
+            //CREATING THE FILE IF IT DOESN'T EXIST USING FILEOUTPUT BECAUSE file.createFile() FAILS :'(;
+            if(fileForDate.exists()) {
+                Log.d(TAG, "Existing file found for date");
+
+                // Read existing data
+                FileInputStream fis = openFileInput(fileName);
+                InputStreamReader isr = new InputStreamReader(fis);
+                BufferedReader br = new BufferedReader(isr);
+                String line;
+                while ((line = br.readLine()) != null) {
+                    jsonContent.append(line);
+                }
+                fis.close();
+
+                // Convert back to object
+                SavePackage save;
+                Gson gson = new Gson();
+                save = gson.fromJson(jsonContent.toString(), SavePackage.class);
+
+                // return our models
+                return save.getTaskModels();
+            }
+        } catch (FileNotFoundException e) {
+            Log.d("FILE", "Failed to create file");
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.d("FILE", "Failed to write file");
+            e.printStackTrace();
+        }
+
+        Log.d(TAG, "No existing file for date");
+        return null;
+    }
+
+    private void displayAllTasks(ArrayList<TaskModel> taskModels) {
+        for (TaskModel tm : taskModels) {
+            taskHolder.addView(new TaskView(getApplicationContext(), null, tm));
+        }
     }
 
     private int findColor() {
@@ -147,94 +211,55 @@ public class CreateActivity extends AppCompatActivity {
         String              title, sDuration, category;
         long                duration;
         int                 color;
-        FileInputStream     fis = null;
-        FileOutputStream    fos = null;
         TaskModel           toSave;
-        StringBuilder       jsonContent;
-        File                file;
+
 
         sDuration   = etDuration.getText().toString();
         title       = etTitle.getText().toString();
         category    = sCategory.getSelectedItem().toString();
         duration    = (sDuration.equals("")) ? 0 : Long.parseLong(sDuration);
         duration    = duration * 1000 * 60;
-        color       = findColor();
-        toSave      = new TaskModel(title, category, duration,
-                        ContextCompat.getColor(getApplicationContext(), color));
+        toSave      = new TaskModel(title, category, duration, selectedColor);
 
         if(title.equals("") || duration == 0) {
             Toast.makeText(getApplicationContext(), "Please enter all fields!", Toast.LENGTH_LONG).show();
             return;
         }
 
-        // TODO: Below line is the fix (and make sure it's just file name now, not file path)
+        tasks.add(toSave);
+        taskHolder.addView(new TaskView(getApplicationContext(), null, toSave), 0);
+
+    }
+
+    public void onColorClick(View view) {
+        colorPicker.show();
+    }
+
+    public void onDoneClick(View view) {
+        FileInputStream     fis = null;
+        FileOutputStream    fos = null;
+        StringBuilder       jsonContent;
+        File                file;
+
         jsonContent = new StringBuilder();
         file        = new File(getApplicationContext().getFilesDir(), fileName);
 
-        /// DEBUG
-        Log.d(TAG, file.getAbsolutePath());
-
-        String[] allFiles = getApplicationContext().fileList();
-        StringBuilder fatString = new StringBuilder();
-        for (String s : allFiles) {
-            fatString.append(s + " ");
-        }
-        Log.d(TAG, fatString.toString());
-
-        if (file.exists()) {
-            Log.d(TAG, "file exists");
-        } else {
-            Log.d(TAG, "file does not exist");
-        }
-        /// end DEBUG
-
         try {
             //CREATING THE FILE IF IT DOESN'T EXIST USING FILEOUTPUT BECAUSE file.createFile() FAILS :'(;
-            if(!file.exists()) {
-                Log.d(TAG, "File does not exist, creating");
+            Log.d(TAG, "File does not exist, creating");
 
-                // Create save package
-                ArrayList<TaskModel> newTm = new ArrayList<>();
-                newTm.add(toSave);
-                SavePackage save = new SavePackage(newTm);
+            // Create save package
+            SavePackage save = new SavePackage(tasks);
 
-                // Serialize new save package
-                Gson gson = new Gson();
-                String content = gson.toJson(save);
+            // Serialize new save package
+            Gson gson = new Gson();
+            String content = gson.toJson(save);
 
-                // Create file
-                fos = openFileOutput(fileName, Context.MODE_PRIVATE);
-                fos.write(content.getBytes());
-                fos.close();
-            } else {
-                Log.d(TAG, "Existing file found, appending");
+            // Create file
+            fos = openFileOutput(fileName, Context.MODE_PRIVATE);
+            fos.write(content.getBytes());
+            fos.close();
 
-                // Read existing data
-                fis = openFileInput(fileName);
-                InputStreamReader isr = new InputStreamReader(fis);
-                BufferedReader br = new BufferedReader(isr);
-                String line;
-                while ((line = br.readLine()) != null) {
-                    jsonContent.append(line);
-                }
-                fis.close();
-
-                // Convert back to object
-                SavePackage save;
-                Gson gson = new Gson();
-                save = gson.fromJson(jsonContent.toString(), SavePackage.class);
-
-                // Add our new task
-                save.setTaskModels(toSave);
-
-                // Reserialize
-                String content = gson.toJson(save);
-
-                // Overwrite file
-                fos = openFileOutput(fileName, Context.MODE_PRIVATE);
-                fos.write(content.getBytes());
-                fos.close();
-            }
         } catch (FileNotFoundException e) {
             Log.d("FILE", "Failed to create file");
             e.printStackTrace();
@@ -246,13 +271,8 @@ public class CreateActivity extends AppCompatActivity {
         Toast.makeText(this, "Task created!", Toast.LENGTH_SHORT).show();
 
         // startActivity(new Intent(this, MainActivity.class));
-    }
 
-    public void onColorClick(View view) {
-        colorPicker.show();
-    }
 
-    public void onDoneClick(View view) {
         startActivity(new Intent(this, MainFlow.class));
     }
 }
